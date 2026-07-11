3d0a3536eb7d6bddeb14350adc6bd7259134f713
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 13:47:00 +0530

    Create 2502-SortThePeople

---
+++ 2502-SortThePeople/2502-SortThePeople.java
@@ -0,0 +1,26 @@
+// Last updated: 7/11/2026, 1:46:59 PM
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        boolean swapped;
+        for(int i=0;i<heights.length;i++){
+            swapped=false;
+            for(int j=0;j<heights.length-i-1;j++){
+                if(heights[j+1]>heights[j]){
+                    int temp=heights[j];
+                    heights[j]=heights[j+1];
+                    heights[j+1]=temp;
+
+                    String stemp=names[j];
+                    names[j]=names[j+1];
+                    names[j+1]=stemp;
+
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        return names;
+    }
+}

341deb1e50558d044d4d71e3a4b34ecbe28120de
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 16:01:57 +0530

    Create 920-UncommonWordsFromTwoSentences

---
+++ 920-UncommonWordsFromTwoSentences/920-UncommonWordsFromTwoSentences.java
@@ -0,0 +1,25 @@
+// Last updated: 7/11/2026, 4:01:56 PM
+import java.util.*;
+
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : s1.split(" ")) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        for (String word : s2.split(" ")) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (String word : map.keySet()) {
+            if (map.get(word) == 1) {
+                result.add(word);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}

6ab85e38221c38daef7cc009e55db8b272cdb272
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 16:02:12 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,30 @@
+// Last updated: 7/11/2026, 4:02:11 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } 
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } 
+            else { 
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}

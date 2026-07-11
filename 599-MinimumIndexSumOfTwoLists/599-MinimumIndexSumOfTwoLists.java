ed9b098c04c7d47fd3e37347bea9feaa00119821
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 16:03:34 +0530

    Create 599-MinimumIndexSumOfTwoLists

---
+++ 599-MinimumIndexSumOfTwoLists/599-MinimumIndexSumOfTwoLists.java
@@ -0,0 +1,27 @@
+// Last updated: 7/11/2026, 4:03:33 PM
+import java.util.*;
+
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        int minSum = Integer.MAX_VALUE;
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    int sum = i + j;
+
+                    if (sum < minSum) {
+                        minSum = sum;
+                        result.clear();
+                        result.add(list1[i]);
+                    } else if (sum == minSum) {
+                        result.add(list1[i]);
+                    }
+                }
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}

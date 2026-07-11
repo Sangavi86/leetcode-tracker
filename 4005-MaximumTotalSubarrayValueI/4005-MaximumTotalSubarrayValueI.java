2d64b8e55f3fad352eb7ba4cde4619dc8439b39d
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 13:46:40 +0530

    Create 4005-MaximumTotalSubarrayValueI

---
+++ 4005-MaximumTotalSubarrayValueI/4005-MaximumTotalSubarrayValueI.java
@@ -0,0 +1,12 @@
+// Last updated: 7/11/2026, 1:46:40 PM
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        long mx=0;
+        long mn=Integer.MAX_VALUE;
+        for (int num:nums) {
+            if (num>mx)mx=num;
+            if (num<mn)mn=num;
+        }
+        return(mx-mn)*1L*k;
+    }
+}

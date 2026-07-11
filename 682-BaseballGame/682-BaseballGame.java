42bd6bef36ccb5d188702e20c225e4e06046e9c0
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 16:03:25 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.java
@@ -0,0 +1,31 @@
+// Last updated: 7/11/2026, 4:03:24 PM
+public class Solution {
+    public int calPoints(String[] ops) {
+        int[] record = new int[ops.length];
+        int index = 0;
+        for (int i = 0; i < ops.length; i++) {
+            String op = ops[i];
+            if (op.equals("C")) {
+                index = index - 1;
+            } else if (op.equals("D")) {
+                int last = record[index - 1];
+                record[index] = last * 2;
+                index = index + 1;
+            } else if (op.equals("+")) {
+                int last = record[index - 1];
+                int secondLast = record[index - 2];
+                record[index] = last + secondLast;
+                index = index + 1;
+            } else {
+                int num = Integer.parseInt(op);
+                record[index] = num;
+                index = index + 1;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < index; i++) {
+            sum = sum + record[i];
+        }
+        return sum;
+    }
+}

523c1a068ae63b2b0e1ea970569dfda1575c9c9c
Sangavi N <sangavi.n86@gmail.com>
2026-07-11 16:01:58 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.java
@@ -0,0 +1,17 @@
+// Last updated: 7/11/2026, 4:01:57 PM
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+    }
+}

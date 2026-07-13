b59c5d91f14295e7b4c3eca08513ef53ee1436ff
Sangavi N <sangavi.n86@gmail.com>
2026-07-13 08:54:24 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+// Last updated: 7/13/2026, 8:54:21 AM
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0; 
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}

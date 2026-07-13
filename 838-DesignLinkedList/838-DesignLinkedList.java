6b9949f73e42340db2f934950f653b0d43588ab8
Sangavi N <sangavi.n86@gmail.com>
2026-07-13 08:50:19 +0530

    Create 838-DesignLinkedList

---
+++ 838-DesignLinkedList/838-DesignLinkedList.java
@@ -0,0 +1,94 @@
+// Last updated: 7/13/2026, 8:50:16 AM
+public class MyLinkedList {
+    private class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+    public void addAtHead(int val) {
+        Node n = new Node(val);
+        if (head == null) {
+            head = n;
+            tail = n;
+        } else {
+            n.next = head;
+            head = n;
+        }
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        Node n = new Node(val);
+        if (head == null) {
+            head = n;
+            tail = n;
+        } else {
+            tail.next = n;
+            tail = n;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+        Node n = new Node(val);
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        n.next = temp.next;
+        temp.next = n;
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+        if (index == 0) {
+            head = head.next;
+            if (size == 1) {
+                tail = null;
+            }
+        } else {
+            Node temp = head;
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (index == size - 1) {
+                tail = temp;
+            }
+        }
+        size--;
+    }
+}

6e0475829c09335bc6f6f6c108d401772b826e73
Sangavi N <sangavi.n86@gmail.com>
2026-07-13 08:52:44 +0530

    Create 289-GameOfLife

---
+++ 289-GameOfLife/289-GameOfLife.java
@@ -0,0 +1,44 @@
+// Last updated: 7/13/2026, 8:52:41 AM
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        int[][] copy = new int[m][n];
+
+        // Copy the board
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = board[i][j];
+            }
+        }
+
+        int[] dx = {-1,-1,-1,0,0,1,1,1};
+        int[] dy = {-1,0,1,-1,1,-1,0,1};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int live = 0;
+
+                for (int k = 0; k < 8; k++) {
+                    int ni = i + dx[k];
+                    int nj = j + dy[k];
+
+                    if (ni >= 0 && ni < m &&
+                        nj >= 0 && nj < n &&
+                        copy[ni][nj] == 1)
+                        live++;
+                }
+
+                if (copy[i][j] == 1) {
+                    if (live < 2 || live > 3)
+                        board[i][j] = 0;
+                } else {
+                    if (live == 3)
+                        board[i][j] = 1;
+                }
+            }
+        }
+    }
+}

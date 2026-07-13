ec493402e06374eec09344e234a6ed7069ca8183
Sangavi N <sangavi.n86@gmail.com>
2026-07-13 08:52:32 +0530

    Create 328-OddEvenLinkedList

---
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+// Last updated: 7/13/2026, 8:52:29 AM
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

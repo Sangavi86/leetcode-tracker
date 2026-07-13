b476721702f846bf3b5df43b19b73cf26dea6d59
Sangavi N <sangavi.n86@gmail.com>
2026-07-13 08:51:23 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,17 @@
+// Last updated: 7/13/2026, 8:51:20 AM
+import java.util.*;
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0; 
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++; 
+            }
+            j++; 
+        }
+        return i; 
+    }
+}

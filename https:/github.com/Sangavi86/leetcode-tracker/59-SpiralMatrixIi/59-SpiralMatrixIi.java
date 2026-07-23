4956e617253fb43e4d2ccfbdc358732302b108e0
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:57:09 +0530

    Create 59-SpiralMatrixIi

---
+++ https:/github.com/Sangavi86/leetcode-tracker/59-SpiralMatrixIi/59-SpiralMatrixIi.java
@@ -0,0 +1,43 @@
+// Last updated: 7/23/2026, 2:57:08 PM
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+        
+        while (top <= bottom && left <= right) {
+            
+            // Left → Right
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+            
+            // Top → Bottom
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+            
+            // Right → Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+            
+            // Bottom → Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+        
+        return matrix;
+    }
+}

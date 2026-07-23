c6c53036a9280a37873db2c0a5c55493f3d11eee
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:04:47 +0530

    Create 643-MaximumAverageSubarrayI

---
+++ https:/github.com/Sangavi86/leetcode-tracker/643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -0,0 +1,19 @@
+// Last updated: 7/23/2026, 3:04:46 PM
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double maxAvg = Double.NEGATIVE_INFINITY;
+
+        for (int i = 0; i <= nums.length - k; i++) {
+            int sum = 0;
+
+            for (int j = i; j < i + k; j++) {
+                sum += nums[j];
+            }
+
+            double avg = (double) sum / k;
+            maxAvg = Math.max(maxAvg, avg);
+        }
+
+        return maxAvg;
+    }
+}

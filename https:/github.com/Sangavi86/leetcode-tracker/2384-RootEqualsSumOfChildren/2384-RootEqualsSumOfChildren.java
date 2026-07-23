c4798b8b084c6b09e6cf167d46443d8d100eb18c
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:02:04 +0530

    Create 2384-RootEqualsSumOfChildren

---
+++ https:/github.com/Sangavi86/leetcode-tracker/2384-RootEqualsSumOfChildren/2384-RootEqualsSumOfChildren.java
@@ -0,0 +1,12 @@
+// Last updated: 7/23/2026, 3:02:04 PM
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) { this.val = val; }
+}
+public class Solution {
+    public boolean checkTree(TreeNode root) {
+        return root.val == root.left.val + root.right.val;
+    }
+}

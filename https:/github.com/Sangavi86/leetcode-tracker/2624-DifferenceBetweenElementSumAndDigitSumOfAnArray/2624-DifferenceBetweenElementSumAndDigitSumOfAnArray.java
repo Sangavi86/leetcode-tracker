47e3acb29c4b3a0672ea7d6c24bfbcfb6c7b6105
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:01:58 +0530

    Create 2624-DifferenceBetweenElementSumAndDigitSumOfAnArray

---
+++ https:/github.com/Sangavi86/leetcode-tracker/2624-DifferenceBetweenElementSumAndDigitSumOfAnArray/2624-DifferenceBetweenElementSumAndDigitSumOfAnArray.java
@@ -0,0 +1,20 @@
+// Last updated: 7/23/2026, 3:01:57 PM
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int sum=0;
+        int digit=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            int n=nums[i];
+        while(n>0){
+            digit=digit + n%10;
+            n=n/10;
+        }
+        }
+        if(sum>digit)
+            return sum-digit;
+        else{
+            return digit-sum;
+        }
+    }
+}

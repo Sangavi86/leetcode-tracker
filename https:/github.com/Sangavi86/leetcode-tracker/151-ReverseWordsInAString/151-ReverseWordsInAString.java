32f957cfd3f587dac1da11e56c7a223d43a47afc
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:48 +0530

    Create 151-ReverseWordsInAString

---
+++ https:/github.com/Sangavi86/leetcode-tracker/151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,11 @@
+// Last updated: 7/23/2026, 2:55:48 PM
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder answer = new StringBuilder();
+        for(int i = words.length-1;i>=0;i--){
+            answer.append(words[i]+" ");
+        }
+        return answer.toString().trim();
+    }
+}

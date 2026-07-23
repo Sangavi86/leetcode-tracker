3bde9ba12b68793f16b540f9483f1e80669f8f3f
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:03:38 +0530

    Create 917-BoatsToSavePeople

---
+++ https:/github.com/Sangavi86/leetcode-tracker/917-BoatsToSavePeople/917-BoatsToSavePeople.java
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 3:03:37 PM
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+    }
+}

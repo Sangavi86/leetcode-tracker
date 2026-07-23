7e8dee5211404be78187d3ca25ab91496074b78d
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:54:19 +0530

    Create 328-OddEvenLinkedList

---
+++ https:/github.com/Sangavi86/leetcode-tracker/328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+// Last updated: 7/23/2026, 2:54:18 PM
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

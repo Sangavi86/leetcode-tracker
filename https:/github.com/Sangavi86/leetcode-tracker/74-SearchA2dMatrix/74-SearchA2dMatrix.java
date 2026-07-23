cb342e920874fc44070d7068ca1f7d96b381868e
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:56:57 +0530

    Create 74-SearchA2dMatrix

---
+++ https:/github.com/Sangavi86/leetcode-tracker/74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,15 @@
+// Last updated: 7/23/2026, 2:56:56 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

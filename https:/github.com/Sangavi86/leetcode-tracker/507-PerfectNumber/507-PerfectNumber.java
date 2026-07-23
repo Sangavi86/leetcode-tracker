e2029109835d8b0cc36852c6745f32ed7d3fc09d
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:05:03 +0530

    Create 507-PerfectNumber

---
+++ https:/github.com/Sangavi86/leetcode-tracker/507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 7/23/2026, 3:05:03 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 1) return false;
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+}

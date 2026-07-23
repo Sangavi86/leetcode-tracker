58fcdeba87a5b62b1056513567aa26a4d77e8d9f
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:04:44 +0530

    Create 667-BeautifulArrangementIi

---
+++ https:/github.com/Sangavi86/leetcode-tracker/667-BeautifulArrangementIi/667-BeautifulArrangementIi.java
@@ -0,0 +1,22 @@
+// Last updated: 7/23/2026, 3:04:43 PM
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        int left = 1;
+        int right = n;
+        int index = 0;
+        while(left<=right){
+            if (k > 1) {
+                if (k % 2 == 1) {
+                    ans[index++] = left++;
+                } else {
+                    ans[index++] = right--;
+                }
+                k--;
+            } else {
+                ans[index++] = left++;
+            }
+        }
+        return ans;
+    }
+}

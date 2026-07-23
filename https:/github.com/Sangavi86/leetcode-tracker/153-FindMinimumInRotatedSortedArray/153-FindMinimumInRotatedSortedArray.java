702ce200351a7755c7f6583869c6edbab50af82c
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:47 +0530

    Create 153-FindMinimumInRotatedSortedArray

---
+++ https:/github.com/Sangavi86/leetcode-tracker/153-FindMinimumInRotatedSortedArray/153-FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 2:55:46 PM
+class Solution {
+    public int findMin(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        while (l < r) {
+            int mid = l+(r-l)/2;
+            if (nums[mid]<nums[r]) {
+                r=mid;
+            }
+            else {
+                l=mid+1;
+            }
+        }
+        return nums[l];
+    }
+}

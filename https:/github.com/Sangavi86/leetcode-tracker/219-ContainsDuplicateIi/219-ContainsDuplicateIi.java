0a4fc20b9960bfdeb92e2fd1552f23cbe5f3bb32
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:22 +0530

    Create 219-ContainsDuplicateIi

---
+++ https:/github.com/Sangavi86/leetcode-tracker/219-ContainsDuplicateIi/219-ContainsDuplicateIi.java
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 2:55:21 PM
+import java.util.HashSet;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}

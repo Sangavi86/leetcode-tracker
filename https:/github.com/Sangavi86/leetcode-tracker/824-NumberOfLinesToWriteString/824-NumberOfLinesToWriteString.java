321fd2c8236f9eb733848589ddec7c54e69d5292
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:04:00 +0530

    Create 824-NumberOfLinesToWriteString

---
+++ https:/github.com/Sangavi86/leetcode-tracker/824-NumberOfLinesToWriteString/824-NumberOfLinesToWriteString.java
@@ -0,0 +1,20 @@
+// Last updated: 7/23/2026, 3:03:59 PM
+class Solution {
+    public int[] numberOfLines(int[] widths, String s) {
+        int lines = 1;
+        int currentWidth = 0;
+
+        for (char ch : s.toCharArray()) {
+            int w = widths[ch - 'a'];
+
+            if (currentWidth + w > 100) {
+                lines++;
+                currentWidth = w;
+            } else {
+                currentWidth += w;
+            }
+        }
+
+        return new int[]{lines, currentWidth};
+    }
+}

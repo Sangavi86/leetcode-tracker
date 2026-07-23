6256a22bf6cd7df5652e2a051222dcfbbb0d267d
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:56 +0530

    Create 137-SingleNumberIi

---
+++ https:/github.com/Sangavi86/leetcode-tracker/137-SingleNumberIi/137-SingleNumberIi.py
@@ -0,0 +1,17 @@
+# Last updated: 7/23/2026, 2:55:56 PM
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                if d[i]==1:
+                    d[i]+=1
+                else:
+                    del d[i]
+        return list(d.keys())[0]
+
+
+
+        

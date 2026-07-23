0318232e08e24cff8b0f65a7d907f63fc2a979d4
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:04:52 +0530

    Create 605-CanPlaceFlowers

---
+++ https:/github.com/Sangavi86/leetcode-tracker/605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -0,0 +1,15 @@
+// Last updated: 7/23/2026, 3:04:51 PM
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n){
+        for(int i=0;i<flowerbed.length;i++){
+            if(flowerbed[i]==0){
+                if((i==0 || flowerbed[i-1]==0) &&
+                   (i==flowerbed.length-1||flowerbed[i+1]==0)){  
+                    flowerbed[i] = 1; 
+                    n--;
+                }
+            }
+        }
+        return n<=0;
+    }
+}

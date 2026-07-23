f0cae982913632483d899b5586934016014510ea
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:10 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ https:/github.com/Sangavi86/leetcode-tracker/238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.java
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 2:55:10 PM
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        ans[0]=1;
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * right;
+            right = right * nums[i];
+        }
+        return ans;
+    }
+}

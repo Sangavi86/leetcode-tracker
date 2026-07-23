f28904eafe3a22969f6e873851f7e84263577c40
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:06 +0530

    Create 242-ValidAnagram

---
+++ https:/github.com/Sangavi86/leetcode-tracker/242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,21 @@
+# Last updated: 7/23/2026, 2:55:05 PM
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d={}
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for j in t:
+            if j not in d:
+                return False
+            else:
+                if d[j]==1:
+                    del d[j]
+                elif d[j]>1:
+                    d[j]-=1
+        if d=={}:
+            return True
+        else:
+            return False

4c92335713543564ac3467e54d40338bc89b2f7a
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:02:55 +0530

    Create 1476-CountNegativeNumbersInASortedMatrix

---
+++ https:/github.com/Sangavi86/leetcode-tracker/1476-CountNegativeNumbersInASortedMatrix/1476-CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,16 @@
+// Last updated: 7/23/2026, 3:02:54 PM
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}

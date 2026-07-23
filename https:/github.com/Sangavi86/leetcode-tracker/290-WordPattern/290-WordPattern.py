4d4345eff5ecbbd4bf94452b7d0fea6c33968fc9
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:54:47 +0530

    Create 290-WordPattern

---
+++ https:/github.com/Sangavi86/leetcode-tracker/290-WordPattern/290-WordPattern.py
@@ -0,0 +1,18 @@
+# Last updated: 7/23/2026, 2:54:46 PM
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+        if len(pattern)!= len(words):
+            return False
+        
+        charToWord ={}
+        wordToChar ={}
+
+        for c,w in zip(pattern,words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+            charToWord[c]=w
+            wordToChar[w]=c
+        return True

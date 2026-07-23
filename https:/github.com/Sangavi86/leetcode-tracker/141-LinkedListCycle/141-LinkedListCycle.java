a00a43d2514a6615663b370fae1b530a7d948ba2
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 14:55:52 +0530

    Create 141-LinkedListCycle

---
+++ https:/github.com/Sangavi86/leetcode-tracker/141-LinkedListCycle/141-LinkedListCycle.java
@@ -0,0 +1,13 @@
+// Last updated: 7/23/2026, 2:55:51 PM
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}

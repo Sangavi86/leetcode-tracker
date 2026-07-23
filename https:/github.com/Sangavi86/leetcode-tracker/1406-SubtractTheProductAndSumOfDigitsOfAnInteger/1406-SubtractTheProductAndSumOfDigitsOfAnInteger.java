c9301b4e60bdd063d18bc0af8f4a4f2471260f9a
Sangavi N <sangavi.n86@gmail.com>
2026-07-23 15:03:03 +0530

    Create 1406-SubtractTheProductAndSumOfDigitsOfAnInteger

---
+++ https:/github.com/Sangavi86/leetcode-tracker/1406-SubtractTheProductAndSumOfDigitsOfAnInteger/1406-SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 7/23/2026, 3:03:02 PM
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product=1;
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            product =product*digit;
+            sum=sum+digit;
+            n=n/10;
+        }
+        return product-sum;
+    }
+}

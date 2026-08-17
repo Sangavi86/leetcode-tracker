8e9f8db6579afc0d4651b69ef561786a79501f4b
Sangavi N <sangavi.n86@gmail.com>
2026-08-17 11:40:40 +0530

    Create 4321-DigitFrequencyScore

---
+++ https:/github.com/Sangavi86/leetcode-tracker/4321-DigitFrequencyScore/4321-DigitFrequencyScore.java
@@ -0,0 +1,12 @@
+// Last updated: 8/17/2026, 11:40:40 AM
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            sum+=digit;
+            n=n/10;
+        }
+        return sum;
+    }
+}

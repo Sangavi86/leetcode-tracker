7b1085950639a97f77f7d33167c16286e98b8f46
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:51:58 +0530

    Create 1662-CheckIfTwoStringArraysAreEquivalent

---
+++ https:/github.com/Sangavi86/leetcode-tracker/1662-CheckIfTwoStringArraysAreEquivalent/1662-CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,14 @@
+// Last updated: 9/7/2026, 9:51:58 AM
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+       String str1="";
+       String str2="";
+       for(String str:word1){
+        str1+=str;
+       }
+       for(String str:word2){
+        str2+=str;
+       }
+       return str1.equals(str2);
+    }
+}

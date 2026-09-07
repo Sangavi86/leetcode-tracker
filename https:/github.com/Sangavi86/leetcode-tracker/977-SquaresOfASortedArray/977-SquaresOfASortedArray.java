de18dc33772b2e5bf5974896dddce2e11e80d061
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:53:23 +0530

    Create 977-SquaresOfASortedArray

---
+++ https:/github.com/Sangavi86/leetcode-tracker/977-SquaresOfASortedArray/977-SquaresOfASortedArray.java
@@ -0,0 +1,10 @@
+// Last updated: 9/7/2026, 9:53:22 AM
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}

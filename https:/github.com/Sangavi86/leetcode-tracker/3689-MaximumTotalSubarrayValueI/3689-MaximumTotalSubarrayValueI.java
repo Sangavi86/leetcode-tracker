3ed15a1bfa0bd2bd0a7faa664b64bf72653b45e7
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:50:12 +0530

    Create 3689-MaximumTotalSubarrayValueI

---
+++ https:/github.com/Sangavi86/leetcode-tracker/3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.java
@@ -0,0 +1,12 @@
+// Last updated: 9/7/2026, 9:50:12 AM
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        long mx=0;
+        long mn=Integer.MAX_VALUE;
+        for (int num:nums) {
+            if (num>mx)mx=num;
+            if (num<mn)mn=num;
+        }
+        return(mx-mn)*1L*k;
+    }
+}

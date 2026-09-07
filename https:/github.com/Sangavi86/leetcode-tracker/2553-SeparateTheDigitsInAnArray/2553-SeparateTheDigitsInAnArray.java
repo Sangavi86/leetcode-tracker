7ba4be08044a51d0957b350a79012793310e6d21
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:50:52 +0530

    Create 2553-SeparateTheDigitsInAnArray

---
+++ https:/github.com/Sangavi86/leetcode-tracker/2553-SeparateTheDigitsInAnArray/2553-SeparateTheDigitsInAnArray.java
@@ -0,0 +1,20 @@
+// Last updated: 9/7/2026, 9:50:52 AM
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> seperatedDigits =new ArrayList<>();
+        
+        for(int i=nums.length-1;i>=0;i--){
+            int n = nums[i];
+            while(n!=0){
+                int digit =n%10;
+                seperatedDigits.add(digit);
+                n/=10;
+            }
+        }
+        int[] results=new int[seperatedDigits.size()];
+        for(int i=0;i<results.length;i++){
+            results[i]=seperatedDigits.get(seperatedDigits.size()-1-i);
+        }
+        return results;
+    }
+}

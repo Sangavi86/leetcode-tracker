9b5d7041b91ca7724077b0f7636726d7a9862f4e
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:54:23 +0530

    Create 821-ShortestDistanceToACharacter

---
+++ https:/github.com/Sangavi86/leetcode-tracker/821-ShortestDistanceToACharacter/821-ShortestDistanceToACharacter.java
@@ -0,0 +1,21 @@
+// Last updated: 9/7/2026, 9:54:23 AM
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+        int n = s.length();
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int minDist = Integer.MAX_VALUE;
+
+            for (int j = 0; j < n; j++) {
+                if (s.charAt(j) == c) {
+                    minDist = Math.min(minDist, Math.abs(i - j));
+                }
+            }
+
+            ans[i] = minDist;
+        }
+
+        return ans;
+    }
+}

ad6a91f859ecce68b06f8051c196c4c79852a0ea
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:51:24 +0530

    Create 1859-SortingTheSentence

---
+++ https:/github.com/Sangavi86/leetcode-tracker/1859-SortingTheSentence/1859-SortingTheSentence.java
@@ -0,0 +1,12 @@
+// Last updated: 9/7/2026, 9:51:24 AM
+class Solution {
+    public String sortSentence(String s) {
+        String[] str=s.split(" ");
+        String[] res=new String[str.length];
+        for(String word: str){
+            int i=word.charAt(word.length()-1)-'0';
+            res[i-1]=word.substring(0,word.length()-1);
+        }
+        return String.join(" ",res);
+    }
+}

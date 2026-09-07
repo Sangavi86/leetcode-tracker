d8c19adc1e71653ae67f9cb17f22ecc00b75a517
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:54:28 +0530

    Create 819-MostCommonWord

---
+++ https:/github.com/Sangavi86/leetcode-tracker/819-MostCommonWord/819-MostCommonWord.java
@@ -0,0 +1,26 @@
+// Last updated: 9/7/2026, 9:54:27 AM
+import java.util.*;
+
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase().replaceAll("[^a-z]", " ");
+        String[] words = paragraph.split("\\s+");
+        HashSet<String> bannedSet = new HashSet<>(Arrays.asList(banned));
+        HashMap<String, Integer> map = new HashMap<>();
+        String result = "";
+        int maxCount = 0;
+        
+        for (String word : words) {
+            if (!bannedSet.contains(word) && word.length() > 0) {
+                map.put(word, map.getOrDefault(word, 0) + 1);
+                
+                if (map.get(word) > maxCount) {
+                    maxCount = map.get(word);
+                    result = word;
+                }
+            }
+        }
+        
+        return result;
+    }
+}

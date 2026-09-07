d627dd4864f85bc4b5071c3a0be6527d467529e1
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:50:41 +0530

    Create 2942-FindWordsContainingCharacter

---
+++ https:/github.com/Sangavi86/leetcode-tracker/2942-FindWordsContainingCharacter/2942-FindWordsContainingCharacter.java
@@ -0,0 +1,14 @@
+// Last updated: 9/7/2026, 9:50:40 AM
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}

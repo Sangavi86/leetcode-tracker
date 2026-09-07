1b22a45fb11b3cfeee72c94d8e3dcf084a5b547c
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:52:46 +0530

    Create 1342-NumberOfStepsToReduceANumberToZero

---
+++ https:/github.com/Sangavi86/leetcode-tracker/1342-NumberOfStepsToReduceANumberToZero/1342-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,15 @@
+// Last updated: 9/7/2026, 9:52:46 AM
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num - 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}

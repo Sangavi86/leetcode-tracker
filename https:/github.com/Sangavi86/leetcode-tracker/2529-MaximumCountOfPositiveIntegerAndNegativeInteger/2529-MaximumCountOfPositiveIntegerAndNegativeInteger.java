097c082b830be3c5ee368de686ad388c5f6f8623
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:50:57 +0530

    Create 2529-MaximumCountOfPositiveIntegerAndNegativeInteger

---
+++ https:/github.com/Sangavi86/leetcode-tracker/2529-MaximumCountOfPositiveIntegerAndNegativeInteger/2529-MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -0,0 +1,21 @@
+// Last updated: 9/7/2026, 9:50:57 AM
+class Solution {
+    public int maximumCount(int[] nums) {
+        int pos=0;
+        int neg=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums [i]<0){
+                neg++;
+            }
+            else if(nums [i]>0){
+                pos++;
+            }
+            }
+        if(pos>neg){
+            return pos;
+        }
+        else{
+            return neg;
+        }
+    }
+}

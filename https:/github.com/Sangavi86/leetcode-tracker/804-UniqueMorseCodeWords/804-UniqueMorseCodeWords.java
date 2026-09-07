e0b958e26d104c4c0a88f9dc5a0f2d9a74487eb7
Sangavi N <sangavi.n86@gmail.com>
2026-09-07 09:54:39 +0530

    Create 804-UniqueMorseCodeWords

---
+++ https:/github.com/Sangavi86/leetcode-tracker/804-UniqueMorseCodeWords/804-UniqueMorseCodeWords.java
@@ -0,0 +1,16 @@
+// Last updated: 9/7/2026, 9:54:38 AM
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String morse[]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+        };
+        HashSet<String>set=new HashSet<>();
+        for(String word:words){
+            String code="";
+            for(char ch:word.toCharArray()){
+                code+=morse[ch-'a'];
+            }
+            set.add(code);
+        }
+        return set.size();
+    }
+}
